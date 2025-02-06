7793cd07507da4bb9efc53360abb660740988f52
Christiaan E. Pretorius <chrisep2@gmail.com>
2025-02-06 11:17:35 +0200

    remove some tidy warnings

--- src/statistics.cpp
+++ src/statistics.cpp
@@ -3,26 +3,26 @@
 /**
  * size stats
  */
-std::atomic<uint64_t> statistics::n4_nodes;
-std::atomic<uint64_t> statistics::n16_nodes;
-std::atomic<uint64_t> statistics::n48_nodes;
-std::atomic<uint64_t> statistics::n256_nodes;
-std::atomic<uint64_t> statistics::node256_occupants;
-std::atomic<uint64_t> statistics::leaf_nodes;
-std::atomic<uint64_t> statistics::node_bytes_alloc;
-std::atomic<uint64_t> statistics::interior_bytes_alloc;
+std::atomic<int64_t> statistics::n4_nodes;
+std::atomic<int64_t> statistics::n16_nodes;
+std::atomic<int64_t> statistics::n48_nodes;
+std::atomic<int64_t> statistics::n256_nodes;
+std::atomic<int64_t> statistics::node256_occupants;
+std::atomic<int64_t> statistics::leaf_nodes;
+std::atomic<int64_t> statistics::node_bytes_alloc;
+std::atomic<int64_t> statistics::interior_bytes_alloc;
 /**
  * ops stats
  */
-std::atomic<uint64_t> statistics::delete_ops;
-std::atomic<uint64_t> statistics::set_ops;
-std::atomic<uint64_t> statistics::iter_ops;
-std::atomic<uint64_t> statistics::iter_start_ops;
-std::atomic<uint64_t> statistics::iter_range_ops;
-std::atomic<uint64_t> statistics::range_ops;
-std::atomic<uint64_t> statistics::get_ops;
-std::atomic<uint64_t> statistics::lb_ops;
-std::atomic<uint64_t> statistics::size_ops;
-std::atomic<uint64_t> statistics::insert_ops;
-std::atomic<uint64_t> statistics::min_ops;
-std::atomic<uint64_t> statistics::max_ops;
+std::atomic<int64_t> statistics::delete_ops;
+std::atomic<int64_t> statistics::set_ops;
+std::atomic<int64_t> statistics::iter_ops;
+std::atomic<int64_t> statistics::iter_start_ops;
+std::atomic<int64_t> statistics::iter_range_ops;
+std::atomic<int64_t> statistics::range_ops;
+std::atomic<int64_t> statistics::get_ops;
+std::atomic<int64_t> statistics::lb_ops;
+std::atomic<int64_t> statistics::size_ops;
+std::atomic<int64_t> statistics::insert_ops;
+std::atomic<int64_t> statistics::min_ops;
+std::atomic<int64_t> statistics::max_ops;

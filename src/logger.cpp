b465596ce63b0e5f25d3cd33dfa09e22d1c491d8
CEP <chrisep2@gmail.com>
2025-04-01 17:53:51 +0200

    working: colorful logging for C++17 although fmtlib looks like a security disaster

--- src/logger.cpp
+++ src/logger.cpp
@@ -5,6 +5,7 @@
 #include "logger.h"
 #include <chrono>
 #include <iostream>
+#include <regex>
 #include <string>
 #include <string_view>
 
@@ -38,20 +39,37 @@ void art::log(const std::string& message)
     std_log(message);
 }
 #else
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Weffc++"
+#include <fmt/core.h>
+#include <fmt/chrono.h>
+#include <fmt/color.h>
+#pragma GCC diagnostic pop
 
+void art::raw_write_to_log(fmt::string_view users_fmt, fmt::format_args&& args)
+{
+    size_t tid = gettid();
+    auto now = std::chrono::system_clock::now();
+    // %d %b %Y %H:%M:%OS
+    auto header_color = fg(fmt::color::orange) | fmt::emphasis::italic;
+    auto text_color = fg(fmt::color::light_blue) | fmt::emphasis::italic;
+    std::string logged = fmt::format(header_color,"{}:M {:%d %b %Y %H:%M:%S} * BARCH ",tid, now);
+    logged += fmt::vformat(text_color, users_fmt, args);
+    std::clog <<  logged << '\n';
+}
 
 void art::log(const std::string& message, const std::exception& e)
 {
-    std::clog << message << " " << e.what() << '\n';
+    std_log(message, e.what());
 }
 
 void art::log(const std::exception& e, const std::string& file, int line)
 {
-    std::clog << e.what() << " " <<  file << " " << line << '\n';
+    std_log(e.what(), file, line);
 }
 void art::log(const std::string& message)
 {
-    std::clog << message << '\n';
+    std_log(message);
 }
 #endif
 
--- src/logger.h
+++ src/logger.h
@@ -8,6 +8,14 @@
 #include <exception>
 #include <array>
 #include <chrono>
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Weffc++"
+#include <fmt/core.h>
+#include <fmt/chrono.h>
+#include <fmt/format.h>
+#include <fmt/color.h>
+#pragma GCC diagnostic pop
+
 namespace art
 {
 #if __cplusplus >= 202002L
@@ -26,10 +34,21 @@ namespace art
         raw_write_to_log(std::string_view{braces.data()}, std::make_format_args(args...));
     }
 #else
+
+
+    extern void raw_write_to_log(fmt::string_view users_fmt, fmt::format_args&& args);
     template<typename... Args>
-    static constexpr void std_log(Args&&... )
+    static constexpr void std_log(Args&&... args)
     {
 
+        // Generate formatting string "{} "...
+        std::array<char, sizeof...(Args) * 3 + 1> braces{};
+        constexpr const char c[4] = "{} ";
+        for (size_t i{0}; i != braces.size() - 1; ++i)
+            braces[i] = c[i % 3];
+        braces.back() = '\0';
+
+        raw_write_to_log(std::string_view{braces.data()}, fmt::make_format_args(args...));
     }
 
 #endif

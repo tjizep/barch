06a5d00c19f2cbfa99fac7f7e1b5298109e9a060
CEP <chrisep2@gmail.com>
2025-04-24 16:45:20 +0200

    not working: fixed a number of issues - still one remaining

--- test/testzrank.lua
+++ test/testzrank.lua
@@ -1,12 +1,17 @@
 local vk
 vk = redis
 
-vk.call('B.CLEAR')
 local t = vk.call('B.MILLIS')
 local count = 1000000
 local key = 'z'
-for i=1,count do
-    vk.call('B.ZADD', key, i, 'at '..i) --i+2.0, 'second' ,i+3.5, 'third'
+if vk.call('B.SIZE') < count then
+    vk.call('B.CLEAR')
+
+    for i=1,count do
+        vk.call('B.ZADD', key, i, 'at '..i) --i+2.0, 'second' ,i+3.5, 'third'
+    end
+    vk.call('B.SAVE')
+
 end
 --6910,58858;2985,75538,4049;857377
 local toadd = vk.call('B.MILLIS') - t
@@ -16,14 +21,15 @@ t = vk.call('B.MILLIS')
 local zr = vk.call('B.ZRANK',key,min,max)
 local torank = vk.call('B.MILLIS') - t
 local zr2 = vk.call('B.ZFASTRANK',key,min,max)
-local range = vk.call('B.ZRANGE',key,min,max)
+--local range = vk.call('B.ZRANGE',key,min,max)
 --assert(#range==zr)
 local tr = {"toadd",toadd,"torank",torank,
     {"min",min},
     {"max",max},
-    {"guess","zr :"..zr,"zr2:"..zr2},
-    {"actual",#range},
-    {"diff",zr-#range},vk.call('B.SIZE')}
-vk.call('B.CLEAR')
+    {"guess","slow zr : "..zr,"fast zr: "..zr2},
+    {"actual 1: "..zr},
+    {"max - min: "..(max-min)},
+    {"diff",zr2-zr},vk.call('B.SIZE')}
+--vk.call('B.CLEAR')
 return tr
 

bab6cee970233c1b4a45ff52fadbf20b49ca83be
CEP <chrisep2@gmail.com>
2025-06-26 14:50:28 +0200

    working: add extra remote tests make setup.py self relative

--- test/repltest.py
+++ test/repltest.py
@@ -6,11 +6,16 @@ k = barch.KeyValue()
 k.set("one","1")
 k.set("two","2")
 k.set("three","3")
+for i in range(1000000):
+    k.set(str(i),str(i))
+for i in range(1000000):
+    k.erase(str(i))
+
 time.sleep(3)
 stats = barch.repl_stats()
 assert(stats.key_add_recv > 0)
 assert(stats.key_add_recv_applied == 0)
-assert(stats.key_rem_recv == 0)
+assert(stats.key_rem_recv > 0)
 assert(stats.key_rem_recv_applied == 0)
 assert(stats.bytes_recv > 0)
 assert(stats.bytes_sent > 0)

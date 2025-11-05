d0d45436e43101a0e200cc25c0e79d6c81f95f12
CEP <chrisep2@gmail.com>
2025-11-05 15:49:09 +0200

    # Release v0.3.4.2b 2025-11-05
    ## New Features
    
    1. Added server_port configuration, 0 disables server if required (v0.3.4.1b 2025-10-01)
    2. Improve client replication io (v0.3.4.1b 2025-10-07)
    3. Make server replication asynch and faster (v0.3.4.1b 2025-10-08)
    4. server replication can be called multiple times per connection (v0.3.4.1b 2025-10-08)
    5. add shard info `INFO SHARD [key]|[#(shard number)]` (v0.3.4.2b 2025-11-05)
       - `INFO SHARD 1` returns shard info for key `1`
       - `INFO SHARD #1` returns shard info for shard number 1
       - `INFO SHARD ##1` or `#ABC` returns shard info for key `##1` or `#ABC`
    6. add key space support with these new api's key space name's should match `[0-9,A-Z,a-z,_]+`
       - `SPACES USE ksname` the named keyspace is made the current store for all other API's, a new keyspace will be empty
       - `SPACES UNLOAD ksname` all memory used by named key space is cleared except if it's in use
       - `SPACES` returns a list of loaded key spaces and their sizes
       - total count in the entire db of all shards in all key spaces
         `SIZEALL`
       - save and clear all key spaces
         `CLEARALL`
         `SAVEALL`
       - Key space operators:
       - `SPACES DEPENDS {depend[e|a]nt key space} ON {source key space name} [STATIC]`
         Let a key space depend on a list of one or more source key spaces (dependant missing keys are resolved in source)
         keys are added to the dependent and not propagated to the source
       - `SPACES RELEASE {depend[e|a]nt key space} FROM {source key space name}`
         release a source from a dependent
       - `SPACES DEPENDANTS {key space name}`
         list the dependants
       - `SPACES DROP {key space name}`
           delete keyspace files when there's no more references to it or on program exit
       - `SPACES MERGE {depend[e|a]nt key space} [INTO {source key space name}]`
          Merge a dependent named key space to its sources or any other random key space
       - `SPACES OPTION [SET|GET] ORDERED [ON|OFF]` sets the current key space to ordered or unordered, option is saved in key space shards
       - `SPACES OPTION [SET|GET] LRU [ON|OFF|VOLATILE]` sets the current key space to evict lru
       - `SPACES OPTION [SET|GET] RANDOM [ON|OFF|VOLATILE]` sets the current key space to evict randomly
       - Key Space random access i.e. `myspace:SET a spaced` or `:GET a` -> `(nil)` or `myspace:GET a` -> `"spaced"`
    - `KSPACE EXIST {key space name} return `1` if space exists else `0`
    8. add optional extra part to storage for future backwards compatibility (v0.3.4.2b 2025-11-05)
    8. Prevent the `KEYS` command from bringing down a server by allowing only one match using the reserved glob thread group (v0.3.4.2b 2025-11-05)
       So keys is still fast but can not bring down a server (v0.3.4.2b 2025-11-05)
    9. `VALUES` match all values in the db against a glob pattern while (v0.3.4.2b 2025-11-05)
    11. Added `CLIENT INFO` command - for resp interface only (v0.3.4.2b 2025-11-05)
    12. Added `MULTI` command - for resp interface only (transaction support in redis style) (v0.3.4.2b 2025-11-05)
    13. Key Space access i.e. `myspace:SET a spaced` (v0.3.4.2b 2025-11-05)

---
+++ test/spacethreadtest.py
@@ -0,0 +1,40 @@
+import threading
+import barch
+import redis
+import time
+
+barch.start("0.0.0.0", 15000)
+gr = redis.Redis(host="127.0.0.0", port=15000, db=0)
+gr.execute_command("USE g")
+gr.set("g","vg")
+def testspace(num):
+    r = redis.Redis(host="127.0.0.0", port=15000, db=0)
+    r.flushdb()
+
+    for i in range(1,1000):
+        r.execute_command(f"USE t{num}")
+        r.execute_command(f"SPACES DEPENDS t{num} ON g")
+        r.set(f"t{i}",f"{i}")
+        assert (r.dbsize() > 0)
+        assert (r.execute_command("g:GET g") == b'vg')
+        assert (r.get("g") == b'vg')
+    assert (r.execute_command(f"SPACES EXIST t{num}") == 1)
+    assert(r.execute_command(f"SPACES DROP t{num}")==b'OK')
+    print(f"exit thread {num}")
+
+t = [
+threading.Thread(target=testspace, args=(1,)),
+threading.Thread(target=testspace, args=(2,)),
+threading.Thread(target=testspace, args=(3,)),
+threading.Thread(target=testspace, args=(4,))
+]
+
+for i in t:
+    i.start()
+
+time.sleep(5)
+
+for i in t:
+    i.join()
+r = redis.Redis(host="127.0.0.0", port=15000, db=0)
+assert(r.execute_command("SPACES EXIST t1") == 0)

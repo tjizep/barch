b093ad916d527cf211e31a55bc8cc991005e1a0c
CEP <chrisep2@gmail.com>
2026-01-20 09:55:06 +0200

    # Release v0.4.3.3b 2025-01-19
    ## New Features
    1. improve conversions (v0.4.3.3b 2025-01-05)
    2. add per key space configuration in the `configuration` key space (v0.4.3.3b 2025-01-06)
       currently only `shards` or shard count is configurable
       ```redis
          SELECT configuration
          SET mys.shards 1
          SELECT mys
          SET foo bar
          SELECT configuration
          GET mys.shards
       ```
    3. add python/hints example (v0.4.3.3b 2025-01-06)
    4. add multi key operations (v0.4.3.3b 2025-01-08)
       `SET "123 abc"` will create a key composed of a number and a string
       `SET "word 12"` can for instance be used to store a text index entry uniquely
    5. add `ordered` to per space configuration (v0.4.3.3b 2025-01-10)
    6. improve allocation (v0.4.3.3b 2025-01-10)
    7. add large geo coding example (v0.4.3.3b 2025-01-12)
    8. add standalone RESP server in python example - with messages passed using lbpop (v0.4.3.3b 2025-01-12)
    9. rework replication (v0.4.3.3b 2025-01-13)
    10. performance improvements (v0.4.3.3b 2025-01-14)
    11. example improvements (v0.4.3.3b 2025-01-14)
    12. add local socket support, just start with unix socket (i.e. `/tmp/barch') on port 0 (v0.4.3.3b 2025-01-17)
    13. add luajit build support interfaces are suitable of for openresty and nginx (v0.4.3.3b 2025-01-17)
    14. add java build and tests (v0.4.3.3b 2025-01-18)
    15. multi thread buffer opt using KeyValue::put (v0.4.3.3b 2025-01-18)
    
    ## Fixes
    1. fix conversions (v0.4.3.3b 2025-01-05)
    2. fix bloom filter bug when dependency is active (v0.4.3.3b 2025-01-06)
    3. fix conversion bugs (v0.4.3.3b 2025-01-09)
    4. fix multi key operations (v0.4.3.3b 2025-01-10)
    5. fix string trimming (v0.4.3.3b 2025-01-10)
    6. fix string decoding (v0.4.3.3b 2025-01-12)
    7. fix `APPEND` and `PREPEND` functions (v0.4.3.3b 2025-01-12)
    8. fix `INCR` and `INCRBY` edge cases, incl rollover/under (v0.4.3.3b 2025-01-13)
    9. fix ordered flag (v0.4.3.3b 2025-01-19)

--- barchj/src/main/java/org/barchtest/Main.java
+++ barchj/src/main/java/org/barchtest/Main.java
@@ -4,6 +4,9 @@ import org.barch.KeyValue;
 import org.barch.barchJNI;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.barch.barchJNI.clearAll;
 
 public class Main {
@@ -13,13 +16,13 @@ public class Main {
         //System.setProperty("java.library.path",path);
         System.loadLibrary("barchjni");
     }
-    static void main() {
+    static void main() throws InterruptedException {
 
         KeyValue conf = new KeyValue("configuration");
         if (conf.size() == 0) {
             System.out.println("creating configuration");
             conf.set("test.ordered", "1");
-            conf.set("test.shards", "1"); // more shards is more concurrency, but only for writes, reads are concurrent- but this is single threaded
+            conf.set("test.shards", "8"); // more shards is more concurrency, but only for writes, reads are concurrent- but this is single threaded
         }else {
             System.out.println("configuration already exists");
 
@@ -29,14 +32,31 @@ public class Main {
         System.out.println("current size:"+kv.size());
         kv.clear();
         long t = System.currentTimeMillis();
-        for (int i = 0; i < 1000000; i++) {
-            String v = Integer.toString(i);
-            kv.put(v, "D"+v);
+        List<Thread> list = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            int finalI = i;
+            Thread vThread = Thread.ofVirtual()
+                .name("my-worker-", finalI) // Optional: prefix and starting index
+                .start(() -> {
+                    KeyValue kvt = new KeyValue("test");
+                    int start = finalI *1000000;
+                    for (int j = start; j < start + 1000000; j++) {
+                        String v = Integer.toString(j);
+                        kvt.put(v, "D"+v);
+                    }
+                });
+            list.add(vThread);
         }
-        System.out.println(System.currentTimeMillis()-t);
+        for (Thread vThread : list){
+            vThread.join();
+        }
+
+
+        System.out.println("time:"+(System.currentTimeMillis()-t));
         System.out.println("min key:" + kv.min().s());
         System.out.println("max key:" + kv.max().s());
         System.out.println("size:" + kv.size() + " 100:" + kv.get("100"));
+        System.out.println("count:" + kv.count("100","10000000"));
         System.out.println("ok");
 
         barchJNI.saveAll();
